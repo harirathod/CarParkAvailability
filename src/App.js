a6890f52b960d2d391875535eb16ae142a30e713
rush212000 <57355106+rush212000@users.noreply.github.com>
2023-08-19 15:26:16 +0100

    Reset

--- src/App.js
+++ src/App.js
@@ -1,56 +1,114 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import backgroundImage from './MandirLDN.jpeg';
 
+const CarPark = ({ name, spaces, showSpaces }) => {
+  const availableSpaces = spaces.filter(space => space.available).length;
 
-// Create 150 spaces for Car Park 1
-const carPark1Spaces = Array.from({ length: 150 }, (_, index) => ({
-  id: index + 1,
-  name: `Car Park Shayona - Space ${String.fromCharCode(65 + index)}`,
-  available: true,
-}));
-const carPark2Spaces = Array.from({ length: 300 }, (_, index) => ({
-  id: index + 1,
-  name: `Car Park Swaminaryan School - Space ${String.fromCharCode(65 + index)}`,
-  available: true,
-}));
+  return (
+    <div className="car-park-card">
+      <div className="card-header">
+        {name}
+      </div>
+      {showSpaces && (
+        <div className="available-spaces">
+          Available spaces: {availableSpaces}
+        </div>
+      )}
+    </div>
+  );
+};
 
+const CarParkBox = ({ carPark }) => {
+  return (
+    <div className="car-park-box">
+      <CarPark
+        name={carPark.name}
+        spaces={carPark.spaces}
+        showSpaces={carPark.showSpaces}
+      />
+    </div>
+  );
+};
 
 const App = () => {
-  const [showCarPark1Spaces, setShowCarPark1Spaces] = useState(false);
-  const [showCarPark2Spaces, setShowCarPark2Spaces] = useState(false);
+  const [showCarPark1Spaces, setShowCarPark1Spaces] = useState(true);
+  const [showCarPark2Spaces, setShowCarPark2Spaces] = useState(true);
+  const [showCarPark3Spaces, setShowCarPark3Spaces] = useState(true);
 
   const toggleCarPark1Spaces = () => {
     setShowCarPark1Spaces(!showCarPark1Spaces);
-    setShowCarPark2Spaces(false); // Close other car park spaces if open
+    setShowCarPark2Spaces(true);
+    setShowCarPark3Spaces(true);
   };
 
   const toggleCarPark2Spaces = () => {
     setShowCarPark2Spaces(!showCarPark2Spaces);
-    setShowCarPark1Spaces(false); // Close other car park spaces if open
+    setShowCarPark1Spaces(true);
+    setShowCarPark3Spaces(true);
   };
 
-  const countAvailableSpaces = (spaces) => {
-    return spaces.filter(space => space.available).length;
+  const toggleCarPark3Spaces = () => {
+    setShowCarPark3Spaces(!showCarPark3Spaces);
+    setShowCarPark1Spaces(true);
+    setShowCarPark2Spaces(true);
+  };
+
+  const carPark1Spaces = Array.from({ length: 150 }, (_, index) => ({
+    id: index + 1,
+    name: `Car Park Shayona - Space ${String.fromCharCode(65 + index)}`,
+    available: true,
+  }));
+
+  const carPark2Spaces = Array.from({ length: 300 }, (_, index) => ({
+    id: index + 1,
+    name: `Car Park Swaminarayan School - Space ${String.fromCharCode(65 + index)}`,
+    available: true,
+  }));
+
+  const carPark3Spaces = Array.from({ length: 200 }, (_, index) => ({
+    id: index + 1,
+    name: `Car Park Tennis Court - Space ${String.fromCharCode(65 + index)}`,
+    available: true,
+  }));
+
+  const carParks = [
+    {
+      name: 'Shayona',
+      spaces: carPark1Spaces,
+      showSpaces: showCarPark1Spaces,
+    },
+    {
+      name: 'Swaminarayan School',
+      spaces: carPark2Spaces,
+      showSpaces: showCarPark2Spaces,
+    },
+    {
+      name: 'Tennis Court',
+      spaces: carPark3Spaces,
+      showSpaces: showCarPark3Spaces,
+    },
+  ];
+
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh', // Fill the whole screen vertically
   };
 
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <header className="header">
         <h1>Car Park Availability</h1>
       </header>
       <main className="main">
-        <div className="buttons">
-          <button onClick={toggleCarPark1Spaces}>Car Park 1</button>
-          <button onClick={toggleCarPark2Spaces}>Car Park 2</button>
-        </div>
-        <div className="spaces">
-          {showCarPark1Spaces && (
-            <div>Available spaces in Car Park Shayona: {countAvailableSpaces(carPark1Spaces)}</div>
-          )}
-          {showCarPark2Spaces && (
-            <div>Available spaces in Car Park Swaminarayan School : {countAvailableSpaces(carPark2Spaces)}</div>
-          )}
+        <div className="car-park-boxes">
+          {carParks.map(carPark => (
+            <CarParkBox key={carPark.name} carPark={carPark} />
+          ))}
         </div>
       </main>
     </div>
@@ -58,4 +116,3 @@ const App = () => {
 };
 
 export default App;
-
